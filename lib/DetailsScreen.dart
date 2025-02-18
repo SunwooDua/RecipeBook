ff7d23319e515b0f72b7ea052976ea7e29021af5
Sunwoo Yeom <sunwooyeom03@gmail.com>
2025-02-18 01:42:09 -0500

    finalized HomeScreen with fixing minor errors like type, established design and successfully received data from homescreen to detailsscreen

--- lib/DetailsScreen.dart
+++ lib/DetailsScreen.dart
@@ -1,1 +1,30 @@
 import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  final Map<String, dynamic> recipe;
+
+  DetailsScreen({required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(recipe['name'])),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text('Ingredients: '),
+              Text(recipe['ingredients']),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Instruction: '),
+              Text(recipe['instructions']),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'DetailsScreen.dart';
 
 class HomeScreen extends StatelessWidget {
-  final List<Map> _Recipes = [
+  final List<Map<String, dynamic>> _recipes = [
     //using List<map> we can take key and value
     {
       'name': 'Soy-sauce Egg Rice',
       'ingredients': 'Soy Sauce, Eggs, Rice, Sesame Oil',
-      'instruction': 'do this and that',
+      'instructions': 'do this and that',
     },
     {
       'name': 'Korean Style Sausage Stir Fry',
       'ingredients':
           'Vienna sausages, Bell Peppers, Onions, Cooking Oil, Ketchup, Soy Sauce, Sugar',
-      'instruction': 'do this and that',
+      'instructions': 'do this and that',
     },
     {
       'name': 'Korean Tuna Mayo Deopbap',
       'ingredients':
           'Soy Sauce, Eggs, Rice, Sesame Oil, Can Tuna, Mayo, Korean Seasoned Seaweed Flakes',
-      'instruction': 'do this and that',
+      'instructions': 'do this and that',
     },
   ];
 
@@ -31,13 +31,21 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: ListView.builder(
-          itemCount: _Recipes.length, // go through every recipes in _Recipes
+          itemCount: _recipes.length, // go through every recipes in _Recipes
           itemBuilder: (context, index) {
             return ListTile(
               leading: Icon(Icons.restaurant_menu), //food icon
-              title: Text(_Recipes[index]
+              title: Text(_recipes[index]
                   ['name']), // only show name of recipe in homescreen
-              onTap: () {}, //when touch recipe show detialed recipe
+              onTap: () {
+                //Navigator.push used to have following action when recipe is pressed
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        //MaterialPageRoute directs to DetailsScreen.dart when recipe is pressed
+                        builder: (context) =>
+                            DetailsScreen(recipe: _recipes[index])));
+              }, //when touch recipe show detialed recipe
               subtitle: Text("click to see detailed information"),
               trailing: Icon(Icons.drag_handle), //drag handle icon at the end
             );
